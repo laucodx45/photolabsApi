a6c75260bb6a16ed96448030c349b57767593e34
Cody <codylau6233@hotmail.com>
2024-02-21 12:16:45 -0800

    Built PhotoListItem to display dummy data.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,15 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <div>
+      <img src={props.data.imageSource} alt="picture" />
+      <img src={props.data.profile} alt="profile picture" />
+      <p>{props.data.username}</p>
+      <p>{props.data.location.city}, {props.data.location.country}</p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
