28f98639d4806468f58bd26cffd856d4eaf1eb89
Cody <codylau6233@hotmail.com>
2024-02-21 16:43:29 -0800

    Refactor PhotoListItem component.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,15 +9,15 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
       <img src={imageSource} alt="picture" className="photo-list__image"/>
-
       <div className="photo-list__user-details">
         <img src={profile} alt="profile picture" className="photo-list__user-profile" />
         <div className="photo-list__user-info">
           <p>{username}</p>
-          <p>{location.city}, <span className="photo-list__user-location">{location.country}</span></p>
+          <div>
+            {location.city}, <span className="photo-list__user-location">{location.country}</span>
+          </div>
         </div>
       </div>
-
     </div>
   );
 };
