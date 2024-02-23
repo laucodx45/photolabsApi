794264ee89bdd9e3fa735abca52ad0be537aa32f
Cody <codylau6233@hotmail.com>
2024-02-22 21:04:51 -0800

    Add TopNavigationBar to App.jsx, move TopicList component into TopNavigationBar, add FavBadge componenet to TopNavigationBar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 // Note: Rendering a single component to build components in isolation
 
 
@@ -12,7 +12,7 @@ const App = () => {
   
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
