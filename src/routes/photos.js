242170efb6a1f5f8349a6daacfdaa0d50d0f7492
Cody <codylau6233@hotmail.com>
2024-06-28 20:53:21 -0700

    Change server url for deployment

--- src/routes/photos.js
+++ src/routes/photos.js
@@ -5,7 +5,7 @@ module.exports = db => {
     const protocol = request.protocol;
     const host = request.hostname;
     const port = process.env.PORT || 8001;
-    const serverUrl = `${protocol}://${host}:${port}`;
+    const serverUrl = `https://photolabsapi.onrender.com`;
 
     db.query(`
       SELECT 
