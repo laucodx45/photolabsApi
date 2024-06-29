b539c7336a50e39d798c6bdb2f52187c0fc8cf9a
Cody <codylau6233@hotmail.com>
2024-06-28 20:24:19 -0700

    Revert to previous version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const PORT = process.env.PORT || 8001;
 const ENV = require("./environment");
-const path = require('path');
 
 const app = require("./application")(ENV);
 const server = require("http").Server(app);
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
